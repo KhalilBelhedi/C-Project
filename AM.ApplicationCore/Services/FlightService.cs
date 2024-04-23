3ec62d6db214618cf5ba0e50b7453938344bb4ec
KhalilBelhedi <80357863+KhalilBelhedi@users.noreply.github.com>
2024-04-23 14:08:25 +0100

    first commit

---
+++ AM.ApplicationCore/Services/FlightService.cs
@@ -0,0 +1,21 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class FlightService : Service<Flight>, IFlightService
+    {
+        public FlightService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {}
+        public IEnumerable<Flight> GetFlightsByDestination(string destination)
+        {
+            return GetMany(e => e.Destination == destination);
+        }
+    }
+}
